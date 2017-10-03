fcfc3cf9541b3c6a799c6c655b28d7359b7a330e
vladimiro <c.vladimiro88@outlook.it>
2017-10-03 12:05:36 +0200

    add css and js libraries

--- AdminPortalWeb/Program.cs
+++ AdminPortalWeb/Program.cs
@@ -20,6 +20,7 @@ namespace AdminPortalWeb
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:4000")
                 .Build();
     }
 }
